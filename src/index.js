a54d7e469f8d1bff39083314617580f6a8fc37bb
vvsos1 <vvsos10@naver.com>
2020-10-27 19:11:00 +0900

    AVL Tree 첫 번째 커밋

--- src/index.js
+++ src/index.js
@@ -1,64 +1,118 @@
 // import * as Treeviz from 'treeviz';
-const Treeviz = require('treeviz');
+const Treeviz = require("treeviz");
 
-let data_1 = [
-    { id: 1, text_1: "Chaos", text_2: "Void", father: null, color:"#FF5722" },
-    { id: 2, text_1: "Tartarus", text_2: "Abyss", father: 1, color:"#FFC107" },
-    { id: 3, text_1: "Gaia", text_2: "Earth", father: 1, color:"#8BC34A" },
-    { id: 4, text_1: "Eros", text_2: "Desire", father: 1,  color:"#00BCD4" }];
+const AVLTree = require("./AVLTree");
 
-let data_2 = [
-    { id: 1, text_1: "Chaos", text_2: " Void", father: null, color:"#2196F3" },
-    { id: 2, text_1: "Tartarus", text_2: "Abyss", father: 1 ,color:"#F44336"},
-    { id: 3, text_1: "Gaia", text_2: "Earth", father: 1,color:"#673AB7" },
-    { id: 4, text_1: "Eros", text_2: "Desire", father: 1,color:"#009688" },
-    { id: 5, text_1: "Uranus", text_2: "Sky", father: 3,color:"#4CAF50" },
-    { id: 6, text_1: "Ourea", text_2: "Mountains", father: 3,color:"#FF9800" }];
 
-    let data_3 = [
-    { id: 1, text_1: "Chaos", text_2: "Void", father: null, color:"#2196F3" },
-    { id: 2, text_1: "Tartarus", text_2: "Abyss", father: 1 ,color:"#F44336"},
-    { id: 3, text_1: "Gaia", text_2: "Earth", father: 1,color:"#673AB7" },
-    { id: 4, text_1: "Eros", text_2: "Desire", father: 1,color:"#009688" },
-    { id: 5, text_1: "Uranus", text_2: "Sky", father: 3,color:"#4CAF50" },
-    { id: 6, text_1: "Ourea", text_2: "Mountains", father: 3,color:"#FF9800" },
-    { id: 7, text_1: "Hermes", text_2: " Sky", father: 4,color:"#2196F3" },
-    { id: 8, text_1: "Aphrodite", text_2: "Love", father: 4,color:"#8BC34A" },
-    { id: 3.3, text_1: "Love", text_2: "Peace", father: 8,color:"#c72e99" },
-    { id: 4.1, text_1: "Hope", text_2: "Life", father: 8,color:"#2eecc7" }
-    ];
-
-let myTree = Treeviz.create({
-    htmlId: "tree",
-    idKey: "id",
-    hasFlatData: true,
-    relationnalField: "father",
-    hasPanAndZoom: true,
-    nodeWidth:120,
-    nodeHeight:80,
-    mainAxisNodeSpacing:2,
-    isHorizontal:false,
-    renderNode: function(node) { 
-    return "<div class='box' style='cursor:pointer;height:"+node.settings.nodeHeight+"px; width:"+node.settings.nodeWidth+"px;display:flex;flex-direction:column;justify-content:center;align-items:center;background-color:"
-            +node.data.color+
-          ";border-radius:5px;'><div><strong>"
-      +node.data.text_1+
-      "</strong></div><div>is</div><div><i>"
-      +node.data.text_2+
-      "</i></div></div>";
-    },
-    linkWidth : (nodeData)=> 5,
-    linkShape:"curve",
-    linkColor : (nodeData) => "#B0BEC5" ,
-    onNodeClick : (nodeData) => console.log(nodeData)
+const myTree = Treeviz.create({
+  htmlId: "tree",
+  idKey: "id",
+  hasFlatData: true,
+  relationnalField: "father",
+  hasPanAndZoom: true,
+  nodeWidth: 120,
+  nodeHeight: 80,
+  mainAxisNodeSpacing: 2,
+  isHorizontal: false,
+  renderNode: function(node) {
+    return (
+      "<div class='box' style='cursor:pointer;height:" +
+      node.settings.nodeHeight +
+      "px; width:" +
+      node.settings.nodeWidth +
+      "px;display:flex;flex-direction:column;justify-content:center;align-items:center;background-color:" +
+      node.data.color +
+      ";border-radius:5px;'><div><strong>" +
+      node.data.text_1 +
+      "</strong></div></div>"
+    );
+  },
+  linkWidth: nodeData => 5,
+  linkShape: "curve",
+  linkColor: nodeData => "#B0BEC5",
+  onNodeClick: nodeData => console.log(nodeData)
 });
-myTree.refresh(data_1);
 
-setTimeout(() => {
-    myTree.refresh(data_2);
+// 해시 함수; AVL 트리 노드의 data 값으로 자신의 Color를 만들기 위해
+function hashFunction(str) {
+  let hash = 0;
+  if (str.length == 0) {
+      return hash;
+  }
+  for (let i = 0; i < str.length; i++) {
+      let char = str.charCodeAt(i);
+      hash = ((hash<<5)-hash)+char;
+      hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+// 숫자를 16진수 color 코드로 변환
+function toColor(num) {
+  num >>>= 0;
+  const b = num & 0xFF,
+      g = (num & 0xFF00) >>> 8,
+      r = (num & 0xFF0000) >>> 16,
+      a = ( (num & 0xFF000000) >>> 24 ) / 255 ;
     
-    setTimeout(() => {
-      myTree.refresh(data_3);
-    }, 1500);
+  let hexColor = "#";
+  for(let color of [a,r,g,b] ) {
+    let hex = color.toString(16);
+    if (hex.length == 1) {
+      hex = "0" + hex;
+    }
+    hexColor+=hex;
+  }
+  return hexColor;
+}
 
-}, 1500);
+// (AVLTreeNode,Integer) -> Array
+// AVLTree를 Treeviz로 시각화 할 수 있는 포맷으로 만들어 반환
+function traversal(root, parentId) {
+  console.log("traversal() : root = " + root);
+  if (root == null || root.isEnd()) return [];
+
+  const id = Number.parseInt(root.data);
+  
+  const data = {
+    id,
+    text_1: root.data,
+    father: parentId,
+    color: toColor(hashFunction(root.data)),
+  };
+
+  return [data]
+    .concat(traversal(root.left, id))
+    .concat(traversal(root.right, id));
+}
+
+// AVL트리의 회전 과정에서 트리 시각화를 하는데 사용되는 함수인 vizCallBack을 만들어주는 함수.
+const vizCallbackMaker = (tree, vizRefreshFunction) => (() => {
+  const datas = traversal(tree.root, null);
+  vizRefreshFunction(datas);
+});
+
+const tree = new AVLTree();
+
+const vizCallback = vizCallbackMaker(tree, myTree.refresh);
+
+// millis만큼 기다린 후 resolve되는 Promise 반환
+const wait = millis =>
+  new Promise(res =>
+    setTimeout(() => {
+      res();
+    }, millis)
+  );
+
+// [50, 30, 75, 10, 40, 60, 80, 20, 55, 65, 95, 70].forEach(i =>
+//   tree.add(i, vizCallback)
+// );
+
+// [50, 30, 75, 10, 40, 60, 80, 20, 55, 65, 95, 70].reduce(
+//   (acc,val) => acc.then(_=>tree.add(val,vizCallback)).then(wait(1000))
+//   ,Promise.resolve());
+
+
+// 0~500까지의 범위의 겹치지 않는 15개의 수를 뽑아 배열로 만든 뒤 이를 AVL Tree에 1초 간격으로 넣어가며 시각화
+new Array(15).fill(1).map(_=>(Math.random()*500).toFixed(0)).filter((elem,idx,arr) => arr.indexOf(elem) == -1 || arr.indexOf(elem) == idx).reduce(
+  (acc,val) => acc.then(_=>tree.add(val,vizCallback)).then(wait(1000))
+  ,Promise.resolve());
