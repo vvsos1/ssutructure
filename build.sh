02059a0964f347de5574ba4605b6cfb185850935
박명규 <vvsos10@naver.com>
2020-10-30 22:43:01 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-rm -r output
+npm install # 라이브러리 설치
+npm install -g browserify # browserify 글로벌로 설치
+rm -r output  # 기존의 output 폴더 삭제
 browserify src/avl-tree/index.js -do output/js/avl-tree/index.js # avl tree 페이지에 들어갈 js를 번들링
 browserify src/bubble-sort/index.js -do output/js/bubble-sort/index.js # bubble sort 페이지에 들어갈 js를 번들링
 browserify src/insertion-sort/index.js -do output/js/insertion-sort/index.js # insertion sort 페이지에 들어갈 js를 번들링
