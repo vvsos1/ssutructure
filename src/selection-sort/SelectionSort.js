bef0e13db4cb685b1acb626bbc1232588c95514f
vvsos1 <vvsos10@naver.com>
2020-11-06 19:30:16 +0900

    SelectionSort2.js 에서 클래스 이름을 SelectionSort2로 변경

--- src/selection-sort/SelectionSort.js
+++ src/selection-sort/SelectionSort.js
@@ -14,23 +14,23 @@ class SelectionSort extends Sort {
     let min;
 
     for (let i = 0; i < n - 1; i += 1) {
-	    min = i;
-	    blocks[i].setColorRed(); //i번째블럭 빨간색으로
+      min = i;
+      blocks[i].setColorRed(); //i번째블럭 빨간색으로
       for (let j = i + 1; j < n; j += 1) {
-	      blocks[j].setColorRed(); // i+1번부터n-1번까지의 블럭을 차례대로 빨간색으로
-        // delay만큼 기다림// 
-	      await new Promise(resolve => setTimeout(resolve, this.delay));
-	      let value1 = blocks[min].getValue(); //변수 설정
-	      let value2 = blocks[j].getValue();
+        blocks[j].setColorRed(); // i+1번부터n-1번까지의 블럭을 차례대로 빨간색으로
+        // delay만큼 기다림//
+        await new Promise(resolve => setTimeout(resolve, this.delay));
+        let value1 = blocks[min].getValue(); //변수 설정
+        let value2 = blocks[j].getValue();
         if (value1 >= value2) min = j;
         if (i != min && j == n - 1) {
           await this.wait();
           await this.swap(blocks[min], blocks[i]); // 블럭 체인지
-		min = i; // min값초기화
-		blocks[min].setColorDefault(); // 위치가 바뀌는  대상블록색깔 파란색으로
+          min = i; // min값초기화
+          blocks[min].setColorDefault(); // 위치가 바뀌는  대상블록색깔 파란색으로
           blocks = this.getBlocks(); //두 블록의 위치가 바뀌었으므로 기존 blocks를 업데이트
         }
-	      await this.wait();
+        await this.wait();
         blocks[j].setColorDefault(); // 빨간색 블럭을 다시 파란색으로
       }
       blocks[i].setColorGreen();
--- src/selection-sort/SelectionSort2.js
+++ src/selection-sort/SelectionSort2.js
@@ -1,42 +1,42 @@
 const Sort = require("../sort/Sort");
 
-class SelectionSort extends Sort {
+class SelectionSort2 extends Sort {
   // container:DOM, delay:Number, animationDelay:Number
   constructor(container, blocks, delay, animationDelay) {
     super(container, blocks, delay, animationDelay);
-}
+  }
 
   async sort() {
     // block들 가져오기
     let blocks = this.getBlocks();
-// block들의 총 개수
+    // block들의 총 개수
     const n = blocks.length;
-let min;
+    let min;
 
-for (let i = 0; i < n - 1; i += 1) {
-            min = i;
-await this.wait();
-blocks[i].setColorRed(); //i번째블럭 빨간색으로
+    for (let i = 0; i < n - 1; i += 1) {
+      min = i;
+      await this.wait();
+      blocks[i].setColorRed(); //i번째블럭 빨간색으로
       for (let j = i + 1; j < n; j += 1) {
-              blocks[j].setColorRed(); // i+1번부터n-1번까지의 블럭을 차례대로 빨간색으로
+        blocks[j].setColorRed(); // i+1번부터n-1번까지의 블럭을 차례대로 빨간색으로
         // delay만큼 기다림//
-              await new Promise(resolve => setTimeout(resolve, this.delay));
-let value1 = blocks[min].getValue(); //변수 설정
-              let value2 = blocks[j].getValue();
-if (value1 >= value2) min = j;
-if (i != min && j == n - 1) {
+        await new Promise(resolve => setTimeout(resolve, this.delay));
+        let value1 = blocks[min].getValue(); //변수 설정
+        let value2 = blocks[j].getValue();
+        if (value1 >= value2) min = j;
+        if (i != min && j == n - 1) {
           await this.swap(blocks[min], blocks[i]); // 블럭 체인지
-                min = i; // min값초기화
-                blocks[min].setColorDefault(); // 위치가 바뀌는  대상블록색깔 파란색으로
+          min = i; // min값초기화
+          blocks[min].setColorDefault(); // 위치가 바뀌는  대상블록색깔 파란색으로
           blocks = this.getBlocks(); //두 블록의 위치가 바뀌었으므로 기존 blocks를 업데이트
         }
         blocks[j].setColorDefault(); // 빨간색 블럭을 다시 파란색으로
       }
       blocks[i].setColorGreen();
-}
+    }
 
     // 정렬이 끝났으므로 마지막 블록도 Green으로 색 변경
     blocks[n - 1].setColorGreen();
+  }
 }
-}
-module.exports = SelectionSort;
+module.exports = SelectionSort2;
