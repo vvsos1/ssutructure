dbc6bc68c817d26d41cab3c008bf4cb0d5759540
김종원 <66167520+HobakDG@users.noreply.github.com>
2020-11-27 18:04:56 +0900

    코드설명 추가

--- src/selection-sort/SelectionSort.js
+++ src/selection-sort/SelectionSort.js
@@ -4,8 +4,21 @@ class SelectionSort extends Sort {
   // container:DOM, delay:Number, animationDelay:Number
   constructor(...args) {
     super(...args);
-
-  }
+    this.drawPseudoCode(  
+      `
+      function SelectionSort(A, n) {
+        for(let i = 0; i < n-1; i++){
+          min = i;
+          for(let j = i+1; j <n; j++){
+            if(A[j] < A[min])
+              min = j;
+          }
+          if(min != i)
+              swap(A[i],A[min])
+        }
+      }`
+          )
+        }
 
   async sort() {
     // 이미 정렬중인 경우 바로 리턴
@@ -30,7 +43,6 @@ class SelectionSort extends Sort {
       blocks[i].setColorRed(); //i번째블럭 빨간색으로
       for (let j = i + 1; j < n;) {
         blocks[j].setColorRed(); // i+1번부터n-1번까지의 블럭을 차례대로 빨간색으로
-        
 
         const {type,memento} = await this.wait();
         // 이전 상태로 복구
@@ -45,24 +57,36 @@ class SelectionSort extends Sort {
             block.setColor(color);
             block.setXPosition(xPosition);
           });
+          this.codeDefault();
 
           continue;
         }
         // 상태 저장
         this.pushMemento({i,j,blocks:[...blocks].map(block=>({...block}))});
-
+        this.codeHighlight(6);
         // delay만큼 기다림//
         await new Promise(resolve => setTimeout(resolve, this.delay));
         let value1 = blocks[min].getValue(); //변수 설정
         let value2 = blocks[j].getValue();
-        if (value1 >= value2) min = j;
+        if(j<n-1){
+          let vcmp = blocks[j+1].getValue();
+          this.setDescription(`${value1}과 ${vcmp} 비교`);
+        }
+        if (value1 >= value2) {
+          this.setDescription(` 현재 최솟값 : ${value2}`);
+          min = j;
+          this.codeHighlight(7);
+        }
         if (i != min && j == n - 1) {
+          this.codeHighlight(9);
+          this.setDescription(`최솟값과 현재 값을 교환한다`);
           await this.swap(blocks[min], blocks[i]); // 블럭 체인지
+          this.codeHighlight(10);
           min = i; // min값초기화
-          blocks[min].setColorDefault(); // 위치가 바뀌는  대상블록색깔 파란색으로
+          blocks[min].setColorDefault(); // 위치가 바뀌는 대상블록색깔 파란색으로
           this.refreshBlocks(); //두 블록의 위치가 바뀌었으므로 blocks를 업데이트
         }
-        blocks[j].setColorDefault(); // 빨간색 블럭을 다시 파란색으로
+        blocks[j].setColorDefault(); // 원래 색깔로 되돌리기
         j += 1;
       }
       blocks[i].setColorGreen();
